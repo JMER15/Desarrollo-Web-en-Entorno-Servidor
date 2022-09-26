d63ba152b71a59d1838c854479de0ed69e1440af
JMER15 <56386242+JMER15@users.noreply.github.com>
2022-09-26 21:27:30 +0200

    Delete ej1_1.php

--- U2/ej1_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @author José Miguel Escribano Ruiz
- */
-  echo "\"Hola Mundo\"";
-?>
-
-<a href=https://github.com/JMER15/Desarrollo-Web-en-Entorno-Servidor/blob/main/U2/ej1_1.php>Código Github</a>
