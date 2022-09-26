cd6e9baf5d1358b810ede31cd489d776affab505
JMER15 <56386242+JMER15@users.noreply.github.com>
2022-09-26 09:30:32 +0200

    Update ej1_1.php

--- U2/ej1_1.php
+++ U2/ej1_1.php
@@ -1,3 +1,8 @@
 <?php
-  echo('Hola Mundo');
+/**
+ * @author José Miguel Escribano Ruiz
+ */
+  echo "\"Hola Mundo\"";
 ?>
+
+<a href=https://github.com/JMER15/Desarrollo-Web-en-Entorno-Servidor/blob/main/U2/ej1_1.php>Código Github</a>
