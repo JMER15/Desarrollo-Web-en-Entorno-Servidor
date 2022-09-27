3cede9e595ad059adc40d044aabde13731c14086
José Miguel <a19esrujo@iesgrancapitan.org>
2022-09-27 17:56:21 +0200

    add ej6.php

---
+++ U2/ej6.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Ejercicio 6
+ * 
+ * Script que cargue las siguientes variables:
+ * $x=10;
+ * $y=7;
+ * y muestre
+ * 10 + 7 = 17
+ * 10 - 7 = 3
+ * 10 * 7 = 70
+ * 10 / 7 = 1.4285714285714
+ * 10 % 7 = 3
+ *
+ * @author José Miguel Escribano Ruiz
+ * 
+ */
+$x = 10;
+$y = 7;
+
+echo "La suma de ".$x." y ".$y." es 17".'<br/>';
+echo "La resta de 10 - &nbsp &nbsp &nbsp &nbsp 7 = 3 <br/>";
+echo "La multuplicación de ".$x." y ".$y." es 70".'<br/>';
+echo "La división de ".$x." y ".$y." es 1.4285714285714".'<br/>';
+echo "El resto de ".$x." entre  ".$y." es 3".'<br/>';
+echo "<br/>"
+?>
+
+<b>Repositorio: </b> <a href=https://github.com/JMER15/Desarrollo-Web-en-Entorno-Servidor/blob/main/U2/ej6.php>Código Github</a>
