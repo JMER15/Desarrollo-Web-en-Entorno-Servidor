7b8770c0303a57f9f4e15628a53f58993d386465
José Miguel <a19esrujo@iesgrancapitan.org>
2022-09-27 17:44:13 +0200

    add ej5.php

---
+++ U2/ej5.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Ejercicio 5
+ * 
+ * Script que escriba el resultado de la suma de dos números almacenados en dos variables.
+ *
+ * @author José Miguel Escribano Ruiz
+ * 
+ */
+$num1 = 8;
+$num2 = 7;
+$suma = $num1+$num2;
+
+echo "La suma de los 2 números es: ".$suma.'<br/>';
+
+?>
+
+<b>Repositorio: </b> <a href=https://github.com/JMER15/Desarrollo-Web-en-Entorno-Servidor/blob/main/U2/ej5.php>Código Github</a>
