d0542c75057f29baf90c2e663b81ad9230f429b5
José Miguel <a19esrujo@iesgrancapitan.org>
2022-09-27 17:34:38 +0200

    add ej4.php

---
+++ U2/ej4.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Ejercicio 4 
+ * 
+ * ¿Cuál es la salida del siguiente script? Prueba el script y modifícalo para las palabras DAW y DWES apararezcan en negrita.
+ * Investiga uso de print y printf para salida de texto.
+ * 
+ * @author José Miguel Escribano Ruiz
+ */
+
+$ciclo="DAW";
+$modulo="DWES";
+print "<p>";
+printf("%s es un módulo de %d curso de %s", "'<b>$modulo</b>'", 2,"'<b>$ciclo</b>'");
+print "</p>";
+?>
+
+<b>Repositorio: </b> <a href=https://github.com/JMER15/Desarrollo-Web-en-Entorno-Servidor/blob/main/U2/ej3.php>Código Github</a>
