cb980e5cb168f615c8570e0711881a7ce7f027f4
José Miguel <a19esrujo@iesgrancapitan.org>
2022-09-28 18:13:28 +0200

    add ejercicios 7,8

---
+++ U3/ejercicio7.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Ejercicio 7
+ * 
+ * 2. Sumar los 3 primeros números pares.
+ * 
+ * @author José Miguel
+ * 
+ * 
+ */
+
+for ($i=1; $i <=3 ; $i++) { 
+    echo $i*2,"<br/>";
+}
+
+echo "<br/>";
+
+?>
+
+<b>Repositorio: </b> <a href=https://github.com/JMER15/Desarrollo-Web-en-Entorno-Servidor/blob/main/U3/ejercicio7.php>Código Github</a>
