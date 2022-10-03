5d8fb455ed282996c7f8f4f18c9ed11a4a2e8113
José Miguel <a19esrujo@iesgrancapitan.org>
2022-10-03 16:23:57 +0200

    add ejercicio9.php y ejercicio10.php

---
+++ U3/ejercicio10.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 
+ * Creación y recorrido de una array
+ * @author josemi
+ * 
+ */
+
+ $contactos= array(
+    array("id"=>1,"nombre"=>'mafalda',"telefono"=>'000000000'),
+    array("id"=>2,"nombre"=>'manolito',"telefono"=>'000000001'),
+    array("id"=>3,"nombre"=>'felipe',"telefono"=>'000000002'),
+ );
+ 
+ foreach ($contactos as $key => $value) {
+   echo $valor["id"]."".$valor["nombre"]."".$telefono["telefono"]."</br>";
+   foreach ($variable as $key => $value) {
+    # code...
+   }
+ }
+
+?>
